99d4a88a415247703b1f3213dd1a714f50e0a709
Lucy <lucy@absolucy.moe>
2023-09-08 08:55:20 -0400

    Minor optimization for frame capture

--- src/frame.rs
+++ src/frame.rs
@@ -12,16 +12,16 @@ use opencv::{
 
 pub fn send_frames(capture: &mut VideoCapture, frame_sender: FrameSender) -> Result<()> {
 	let mut frame_num = 1_usize;
+	let mut raw_frame = Mat::default();
 	loop {
-		let mut frame = Mat::default();
 		if !capture
-			.read(&mut frame)
+			.read(&mut raw_frame)
 			.wrap_err("failed to read frame from video capture")?
 		{
 			break;
 		}
 		let mut grey_frame = Mat::default();
-		opencv::imgproc::cvt_color(&frame, &mut grey_frame, COLOR_BGR2GRAY, 0)
+		opencv::imgproc::cvt_color(&raw_frame, &mut grey_frame, COLOR_BGR2GRAY, 0)
 			.wrap_err("failed to convert frame to greyscale")?;
 		frame_sender
 			.send((grey_frame, frame_num))
