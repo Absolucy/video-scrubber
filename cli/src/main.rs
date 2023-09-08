715b4988639fa7e36c0b624c9bfd7fbfc3c81530
Lucy <lucy@absolucy.moe>
2023-09-08 18:33:50 -0400

    actually implement main lol

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,7 +1,18 @@
 pub mod cmd;
 pub mod scrub;
 
+use self::cmd::{CliArgs, CliSubcommands};
+use clap::Parser;
+use color_eyre::eyre::Result;
+
 #[global_allocator]
 static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
 
-fn main() {}
+fn main() -> Result<()> {
+	color_eyre::install()?;
+	let args = CliArgs::parse();
+	match args.command {
+		CliSubcommands::Scrub(args) => scrub::scrub(args),
+		CliSubcommands::Test(_args) => todo!(),
+	}
+}
